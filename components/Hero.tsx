671312545ab2555732643597e7b124a27e28a321
Bernardo <bernardo.rshaid@bue.edu.ar>
2026-08-11 15:32:53 -0300

    Remove 'Scroll' text from hero - arrow animation is enough

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -104,7 +104,6 @@ export default function Hero() {
         transition={{ delay: 1.5, duration: 0.5 }}
         className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-2 text-white/30"
       >
-        <span className="text-xs tracking-widest uppercase">Scroll</span>
         <motion.div
           animate={{ y: [0, 6, 0] }}
           transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
